70fcb1b138d34b54c7153ec93a2c34191a85b1af
HansBherer[Desktop] <77289920+HansBherer@users.noreply.github.com>
2021-05-28 12:55:08 -0400

    add PWA

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
   pwa: {
     display: 'standalone',
     background_color: '#F2F3F7',
-    themeColor: '#047668',
+    themeColor: '#058373',
     msTileColor: '#000000',
     backgroundColor: '#F2F3F7',
-    theme_color: '#047668',
+    theme_color: '#058373',
     manifestOptions: {
       name: 'ListeÉpicerie',
       short_name: 'LÉ'
